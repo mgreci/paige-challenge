9a7d31ee9b9e26373f5ebfe4074fb34c8baa143e
Matthew Greci <matthew@greci.tech>
2023-10-19 15:21:44 -0600

    fix ui and key generation

--- src/app/product-list/page.tsx
+++ src/app/product-list/page.tsx
@@ -3,23 +3,31 @@
 import { ProductContext } from "@/app/product.context";
 import Link from "next/link";
 import { useContext } from "react";
+import { IProduct } from "../product.interface";
+import classes from './page.module.css';
+
+// NOTE: given product-fixtures.json has two items with the same id.
+// no details on what is guaranteed to be unique or not so I will generate key from both id and sku for safety.
+const generateProductKey = (id: string, sku: string) => `${id}-${sku}`;
 
 export default function ProductList({ }) {
-	const { products } = useContext(ProductContext);
+	const { products }: { products: IProduct} = useContext(ProductContext)!;
 	return (
 		<>
 			<h1>Product List</h1>
-			<table>
+			<table className={classes.table}>
 				<thead>
-					<th>Name</th>
-					<th>Color</th>
-					<th>Type</th>
-					<th>Cost</th>
-					<th></th>
+					<tr>
+						<th>Name</th>
+						<th>Color</th>
+						<th>Type</th>
+						<th>Cost</th>
+						<th></th>
+					</tr>
 				</thead>
 				<tbody>
-					{products?.map(({ name, color, type, price, sku }) => (
-						<tr key={sku}>
+					{products?.map(({ id, name, color, type, price, sku }) => (
+						<tr key={generateProductKey(id, sku)}> 
 							<td>{name}</td>
 							<td>{color}</td>
 							<td>{type}</td>
