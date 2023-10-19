7aac4f181010a64c3d73d3aa7de83fe6aec0f37d
Matthew Greci <matthew@greci.tech>
2023-10-19 15:59:48 -0600

    update product

--- src/app/product-detail/[sku]/page.tsx
+++ src/app/product-detail/[sku]/page.tsx
@@ -1,20 +1,37 @@
 'use client';
 
 import { ProductContext } from "@/app/product.context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import classes from "./page.module.css";
 import { IProduct } from "@/app/product.interface";
+import { useRouter } from "next/navigation";
 
 export default function ProductDetail({ params: { sku } }: { params: { sku: string } }) {
-	const { searchBySku } = useContext(ProductContext);
+	const router = useRouter();
+	const { searchBySku, update } = useContext(ProductContext);
 	const product: IProduct  = searchBySku(sku)!;
 	const { name, type, description, color, price } = product;
 
+	const [modifiedProduct, setModifiedProduct] = useState(product);
+
+	const handleProductChange = (property: string, value: string|number) => {
+		setModifiedProduct({
+			...modifiedProduct,
+			[property]: value
+		})
+	};
+
 	const handleUpdate = () => {
 		// obtain form values
+
 		// validate
+
+		// update product
+		update(product.id, modifiedProduct);
+
 		// redirect to product list page
+		router.push('/product-list');
 	};
 
 	return (
@@ -22,15 +39,15 @@ export default function ProductDetail({ params: { sku } }: { params: { sku: stri
 			<h1>Product: {sku}</h1>
 			<form className={classes.productDetail}>
 				<label className={classes.detailLabel} htmlFor="name">Name</label>
-				<input id="name" type="text" defaultValue={name} />
+				<input id="name" type="text" defaultValue={name} onChange={event => handleProductChange("name", event.target.value)}/>
 				<label className={classes.detailLabel} htmlFor="type">Type</label>
-				<input id="type" type="text" defaultValue={type} />
+				<input id="type" type="text" defaultValue={type} onChange={event => handleProductChange("type", event.target.value)}/>
 				<label className={classes.detailLabel} htmlFor="description">Description</label>
-				<input id="description" type="text" defaultValue={description} />
+				<input id="description" type="text" defaultValue={description} onChange={event => handleProductChange("description", event.target.value)}/>
 				<label className={classes.detailLabel} htmlFor="color">Color</label>
-				<input id="color" type="text" defaultValue={color} />
+				<input id="color" type="text" defaultValue={color} onChange={event => handleProductChange("color", event.target.value)}/>
 				<label className={classes.detailLabel} htmlFor="price">Price</label>
-				<input id="price" type="number" step="0.01" defaultValue={price} />
+				<input id="price" type="number" step="0.01" defaultValue={price} onChange={event => handleProductChange("price", event.target.value)}/>
 			</form>
 			<button type="button" className={classes.update} onClick={handleUpdate}>Update</button>
 		</>
--- src/app/product.context.js
+++ src/app/product.context.js
@@ -3,16 +3,29 @@
 import { createContext } from 'react';
 
 import products from './product-fixtures.json';
+// NOTE: possible typo in that there are two objects with the same id value.
 
 const searchBySku = (searchSku) => {
   return products?.find(({ sku }) => searchSku === sku);
 }
 
-export const ProductContext = createContext({ products: [{}], searchBySku });
+const update = (productId, newProduct) => {
+  // validate newProduct?
+
+  // find index
+  const index = products?.findIndex(({ id }) => id === productId);
+
+  // if index exists, update
+  if (index > -1) {
+    products.splice(index, 1, newProduct);
+  }
+};
+
+export const ProductContext = createContext({ products: [{}], searchBySku, update });
 
 export const ProductContextProvider = ({ children }) => {
   return (
-    <ProductContext.Provider value={{ products, searchBySku }}>
+    <ProductContext.Provider value={{ products, searchBySku, update }}>
 			{children}
 		</ProductContext.Provider>
   );
