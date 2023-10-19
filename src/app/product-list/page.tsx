78e6db2486f988934f4ce9432f32c8a7e8e8f3ca
Matthew Greci <matthew@greci.tech>
2023-10-19 14:13:49 -0600

    replace strinify with basic table

--- src/app/product-list/page.tsx
+++ src/app/product-list/page.tsx
@@ -1,14 +1,45 @@
 'use client';
 
 import { ProductContext } from "@/app/product.context";
+import Link from "next/link";
 import { useContext } from "react";
+
+// interface IProduct {
+// 	name: string,
+// 	color: string,
+// 	type: string,
+// 	price: number,
+// 	sku: string,
+// };
 
 export default function ProductList({ }) {
 	const { products } = useContext(ProductContext);
 	return (
 		<>
 			<h1>Product List</h1>
-			<code>{JSON.stringify(products, null, 2)}</code>
+			<table>
+				<thead>
+					<th>Name</th>
+					<th>Color</th>
+					<th>Type</th>
+					<th>Cost</th>
+					<th></th>
+				</thead>
+				<tbody>
+					{products?.map(({ name, color, type, price, sku }) => (
+						<tr key={sku}>
+							<td>{name}</td>
+							<td>{color}</td>
+							<td>{type}</td>
+							<td>{price}</td>
+							<td>
+								<button type="button">Edit</button>
+								<button type="button"><Link href={`/product-detail/${sku}`}>View</Link></button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	)
 };
